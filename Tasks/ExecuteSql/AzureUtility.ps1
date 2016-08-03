aeac5c5ade811c6b8301626e94cfcff6316cb040
Adrien Siffermann <adrien@siffermann.me>
2016-08-03 13:25:30 +0200

    ExecuteSql: Change AzureUtility

--- Tasks/ExecuteSql/AzureUtility.ps1
+++ Tasks/ExecuteSql/AzureUtility.ps1
@@ -12,58 +12,6 @@ function Get-AgentStartIPAddress
         throw (Get-VstsLocString -Key "Cannot retrieve Agent Start IP")
     }
 
-
-    # $endpoint = Get-VstsEndpoint -Name SystemVssConnection
-    # $credentials = Get-VstsVssCredentials
-    
-    # $agentWorkerModulesPath = "$($env:AGENT_HOMEDIRECTORY)\agent\worker"    
-
-    # $SystemNetHttpFormatting = [reflection.assembly]::LoadFrom("$agentWorkerModulesPath\System.Net.Http.Formatting.dll")
-    # $NewtonsoftJson = [reflection.assembly]::LoadFrom("$agentWorkerModulesPath\Newtonsoft.Json.dll")
-
-    # $OnAssemblyResolve = [System.ResolveEventHandler] {
-    #     param($sender, $e)
-
-    #     if ($e.Name.StartsWith("Newtonsoft.Json"))
-    #     { 
-    #         return $NewtonsoftJson 
-    #     }
-
-    #     if ($e.Name.StartsWith("System.Net.Http.Formatting"))
-    #     {
-    #         return $SystemNetHttpFormatting
-    #     }
-
-    #     foreach ($a in [System.AppDomain]::CurrentDomain.GetAssemblies())
-    #     {
-    #         if ($a.FullName -eq $e.Name)
-    #         {
-    #             return $a
-    #         }
-    #     }
-
-    #     return $null
-    # }
-
-    # [System.AppDomain]::CurrentDomain.add_AssemblyResolve($OnAssemblyResolve)
-
-    # [reflection.assembly]::LoadFrom("$agentWorkerModulesPath\Microsoft.TeamFoundation.DistributedTask.Agent.Interfaces.dll")
-    # [reflection.assembly]::LoadFrom("$agentWorkerModulesPath\Microsoft.VisualStudio.Services.WebApi.dll")
-    # [reflection.assembly]::LoadFrom("$agentWorkerModulesPath\Microsoft.TeamFoundation.DistributedTask.Agent.Common.dll")
-    # [reflection.assembly]::LoadFrom("$agentWorkerModulesPath\Microsoft.VisualStudio.Services.Common.dll")
-    
-    # Import-Module "$agentWorkerModulesPath\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Internal\Microsoft.TeamFoundation.DistributedTask.Task.Internal.dll"
-    # Import-Module "$agentWorkerModulesPath\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Common\Microsoft.TeamFoundation.DistributedTask.Task.Common.dll"
-    # Import-Module "$agentWorkerModulesPath\Modules\Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs\Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs.dll"
-
-    # $connection = New-Object Microsoft.VisualStudio.Services.WebApi.VssConnection -ArgumentList @($endpoint.Url, $credentials)
-
-    # # getting start ip address from dtl service
-    # Write-Verbose "Getting external ip address by making call to dtl service"
-    # $startIP = Get-ExternalIpAddress -Connection $connection
-
-    # [System.AppDomain]::CurrentDomain.remove_AssemblyResolve($OnAssemblyResolve)
-
     return $startIP
 }
 
@@ -90,24 +38,24 @@ function Get-AgentIPAddress
 
 function Get-AzureUtility
 {
-    $currentVersion =  Get-AzureCmdletsVersion
-    Write-Verbose "Installed Azure PowerShell version: $currentVersion"
+    $currentVersion =  (Get-Module -Name AzureRM.profile).Version
+    Write-Verbose  "Installed Azure PowerShell version: $currentVersion"
 
     $minimumAzureVersion = New-Object System.Version(0, 9, 9)
-    $versionCompatible = Get-AzureVersionComparison -AzureVersion $currentVersion -CompareVersion $minimumAzureVersion
-
+	
     $azureUtilityOldVersion = "AzureUtilityLTE9.8.ps1"
     $azureUtilityNewVersion = "AzureUtilityGTE1.0.ps1"
-
-    if(!$versionCompatible)
+	
+	
+    if( $currentVersion -and $currentVersion -gt $minimumAzureVersion )
     {
-        $azureUtilityRequiredVersion = $azureUtilityOldVersion
+	    $azureUtilityRequiredVersion = $azureUtilityNewVersion  
     }
     else
     {
-        $azureUtilityRequiredVersion = $azureUtilityNewVersion
+	    $azureUtilityRequiredVersion = $azureUtilityOldVersion
     }
-
+	
     Write-Verbose "Required AzureUtility: $azureUtilityRequiredVersion"
     return $azureUtilityRequiredVersion
 }
