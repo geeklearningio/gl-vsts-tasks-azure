3e9b00872a591f8917acee227983bba280d9b958
Adrien Siffermann <adrien@siffermann.me>
2016-09-11 23:11:28 +0200

    Add test for SqlMultiDacpacDeployment with only one DACPAC file

--- Tests/PowerShell3/GeekLearning.VstsTasks.Azure.Tests/SqlMultiDacpacDeployment.Tests.ps1
+++ Tests/PowerShell3/GeekLearning.VstsTasks.Azure.Tests/SqlMultiDacpacDeployment.Tests.ps1
@@ -29,6 +29,23 @@ Describe "SqlMultiDacpacDeployment" {
 			$outputVariable | Should Not BeLike "*task.logissue type=error*"
 		}
 
+        It "Runs with only one DACPAC" {
+            Add-VstsInput -name "DacpacFiles" -value $config.settings.SqlMultiDacpacDeployment.OneDacpacFile.Replace("`$(Build.SourcesDirectory)", $here)
+            Add-VstsInput -name "AdditionalArguments" -value $config.settings.SqlMultiDacpacDeployment.AdditionalArguments
+            Add-VstsInput -name "ServerName" -value $config.settings.SqlMultiDacpacDeployment.ServerName
+            Add-VstsInput -name "DatabaseName" -value $config.settings.SqlMultiDacpacDeployment.DatabaseName
+            Add-VstsInput -name "SqlUsername" -value $config.settings.SqlMultiDacpacDeployment.SqlUsername
+            Add-VstsInput -name "SqlPassword" -value $config.settings.SqlMultiDacpacDeployment.SqlPassword
+            Add-VstsInput -name "IpDetectionMethod" -value $config.settings.SqlMultiDacpacDeployment.IpDetectionMethod
+            Add-VstsInput -name "DeleteFirewallRule" -value $config.settings.SqlMultiDacpacDeployment.DeleteFirewallRule.ToString()
+
+			$outputVariable = Invoke-VstsTaskScript -ScriptBlock ([scriptblock]::Create(". $($config.targetScriptPath)")) -Verbose 2>&1 3>&1 4>&1 5>&1 6>&1 | Out-String
+			$outputVariable | Out-File -FilePath "$here/dump.txt" -Force
+			Write-VstsAddAttachment -Type "TestLog" -Name "SqlMultiDacpacDeployment-Runs" -Path "$here/dump.txt"
+			
+			$outputVariable | Should Not BeLike "*task.logissue type=error*"
+		}
+
 		It "Fails because no DACPAC is found" {
             Add-VstsInput -name "DacpacFiles" -value "whatever\\*.any"
             Add-VstsInput -name "AdditionalArguments" -value $config.settings.SqlMultiDacpacDeployment.AdditionalArguments
